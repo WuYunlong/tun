5c5b84676054cba3e47368b99ec683bbcc61df18
Mac <mac@MacdeMacBook-Pro.local>
2024-07-15 23:22:38 +0800

    添加tcp多路复用框架

--- cmd/tunc/main.go
+++ cmd/tunc/main.go
@@ -1,4 +1,48 @@
 package main
 
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"github.com/wuyunlong/tun/pkg/version"
+	"os"
+)
+
 func main() {
+	Execute()
+}
+
+var (
+	showVersion bool
+)
+
+func init() {
+	rootCmd.PersistentFlags().BoolVarP(&showVersion, "version", "v", false, "show version")
+}
+
+var rootCmd = &cobra.Command{
+	Use:   "tunc",
+	Short: "tunc is the client of tun (https://github.com/WuYulong/tun)",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if showVersion {
+			fmt.Println(version.Full())
+			return nil
+		}
+		if err := runClient(); err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		return nil
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func runClient() error {
+	fmt.Println("run client ...")
+	return nil
 }
